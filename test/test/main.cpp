05f95a7c482d38bc5a9889a2bf2ea9ed01fb5e9c
enniscai <enniscai@qq.com>
2017-03-26 18:24:04 +0800

    3333

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by enniscai on 17/3/26.
+//  Copyright © 2017年 enniscai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
